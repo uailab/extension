d1d357987d20183549daa0077d60bd946db3abd4
odutradev <joao.vitornl@gmail.com>
2024-11-29 11:21:45 -0300

    feat: Configure typescript build and start

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,10 @@
-import gulog from "gulog"; 
-
 import app  from "./app";
 
-gulog.setup({
-    prefix: '(UaiLab API)'
-});
-
 const server = app.listen(1000, async () => {
-    console.log(server)
-    gulog.info(`🚀 Server iniciado na porta: 1000`)
+    console.log(`🚀 Server iniciado na porta: 1000`)
 });
 
 process.on('SIGINT', () => {
-    gulog.error("Server encerrado")
+    console.log("Server encerrado")
     server.close();
 });
