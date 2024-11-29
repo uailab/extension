fec9d2fa35fb16bd847ebea0b7d577161a33b848
odutradev <joao.vitornl@gmail.com>
2024-11-29 11:44:34 -0300

    feat: Update listen event

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,11 @@
 import app  from "./app";
 
-const server = app.listen(1000, async () => {
-    console.log(`🚀 Server iniciado na porta: 1000`)
+const server = app.listen(process.env.PORT, async () => {
+    const address = server.address();
+    if (typeof address === 'object' && address !== null) {
+        const host = address.address === '::' ? 'localhost' : address.address;
+        console.log(`🚀 Server iniciado em: http://${host}:${address.port}`);
+    };
 });
 
 process.on('SIGINT', () => {
