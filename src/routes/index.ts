7581917aeed061a58edc37bd158e78103ab295cf
odutradev <joao.vitornl@gmail.com>
2024-12-02 10:08:10 -0300

    feat: Create users router

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,13 @@
 import { Router } from "express";
 
+import usersRouter from "./resources/user.router";
+
 const router = Router();
 
-router.get("ping", (req, res) => {
+router.get("/ping", (req, res) => {
     res.sendStatus(200);
 });
+
+router.use("/users", usersRouter);
 
 export default router;
