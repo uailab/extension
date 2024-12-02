6d97f0ca1a7094876c8c3d57c081a5a48796a136
odutradev <joao.vitornl@gmail.com>
2024-12-02 18:24:21 -0300

    feat: Create resource path alias

--- src/routes/resources/user.router.ts
+++ src/routes/resources/user.router.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 
-import usersResource from "resources/users/users.resource";
+import usersResource from "@resources/users/users.resource";
 import manageRequest from "@middlewares/manageRequest";
 import auth from "@middlewares/auth";
 
